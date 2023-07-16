d98bc79fcd02b00b3340564d021e67d9f8ff153b
Herve Nicol <12008875+hervenicol@users.noreply.github.com>
2023-07-16 12:08:49 +0200

    reworked upcoming_events plugin, still having deprecation alerts

--- _plugins/upcoming_events.rb
+++ _plugins/upcoming_events.rb
@@ -3,13 +3,17 @@ module Reading
     VERSION = '0.1.0'
 
     def generate(site)
-      site.collections.each do |name, collection|
-        if name == 'events'
-          # site.data['upcomingEvents'] = collection.docs.select { |event| event.data['start'] >= Time.now }
-          # site.data['upcomingEvents'] = collection.docs {|content|}
-          site.data['upcomingEvents'] = collection.docs.select {|content| content.start >= Time.now }
-          site.data['pastEvents'] = collection.docs.select {|content| content.end < Time.now }
-        end
+      # Init new data categories
+      site.data['upcomingEvents'] ||= []
+      site.data['pastEvents'] ||= []
+
+      # Loop through events
+      site.collections['events'].docs.each do |event|
+          if event.start >= Time.now
+            site.data['upcomingEvents'] << event
+          else
+            site.data['pastEvents'] << event
+          end
       end
     end
 
